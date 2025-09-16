ddaf205d71bd391b6981157d21769f9441a63edc
aleja <aleja@DESSIREE>
2025-09-16 12:44:36 -0400

    Add project files.

---
+++ UnitConversionCalc/Program.cs
@@ -0,0 +1,10 @@
+﻿// Week 4 Assignment_UnitConversionCalc -- Alejandra Arauz
+using static System.Console;
+
+class ApplicationRunning
+{
+    public static void Main(string[] args)
+    {
+
+    }
+}
